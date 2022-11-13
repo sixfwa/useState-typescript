40618b7103996cfc4737c926ca1d02cfe5ac7306
Farhan Ali <sixfwali@gmail.com>
2022-11-13 13:47:14 +0100

    useState example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+/*
+  1. You can only use hooks inside of funcitonal components
+  2. You cannot write hooks inside of conditional statements
+**/
+
+const App = () => {
+  /*
+    useState returns the current state which in this case
+    is `movies` and a function that allows us to update 
+    the state, `setMovies`. Typically named like so.
+  **/
+
+  const [movies, setMovies] = useState<Array<string>>([
+    "The Godfather",
+    "Harry Potter and the Chamber of Secrets",
+    "Toy Story 1",
+    "Gladiator",
+  ]);
+
+  const [newMovie, setNewMovie] = useState<string>("");
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>): void => {
+    setNewMovie(event.currentTarget.value);
+  };
+
+  const addNewMovie = (): void => {
+    setMovies((movies) => [...movies, newMovie]);
+    setNewMovie("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>List of movies</h1>
+      <input type="text" value={newMovie} onChange={onChange} />
+      <button onClick={addNewMovie}>Add Movie</button>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
